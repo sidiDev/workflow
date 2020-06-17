f72cfcdcaf8842e1dfa961a2c0742c2b12505f92
sidiDev <sidiDev3@gmail.com>
2020-06-17 23:10:21 +0300

    function to close navbar when click outside it

--- javaScript files/home.js
+++ javaScript files/home.js
@@ -13,20 +13,6 @@ for (let i = 0; i < navLinks.length -1; i++) {
     }
 }
 
-// Get navbar
-let navbar = document.querySelector('nav');
-
-// When window scroll > 20 add class in navbar
-window.onscroll = function() {
-
-    if (this.scrollY > 100) {
-        navbar.classList.add('fixed-nav')
-
-    } else {
-        navbar.classList.remove('fixed-nav')
-    }
-}
-
 //Get menu button
 let munBtn = document.querySelector('.menu-btn');
 
@@ -34,7 +20,37 @@ let munBtn = document.querySelector('.menu-btn');
 let navContainer = document.querySelector('.navbar-container')
 let navContainer_2 = document.querySelector('.nav-container')
 
+// Get navbar
+let navbar = document.querySelector('nav');
+
+// When window scroll > 20 add class in navbar
+window.onscroll = function() {
+    navContainer.classList.remove('open-close-nav');
+    navContainer_2.classList.remove('open-close-nav'); 
+
+    if (this.scrollY > 100) {
+        navbar.classList.add('fixed-nav');
+        navbar.classList.add('shadow');
+
+    } else {
+        navbar.classList.remove('fixed-nav');
+        navbar.classList.remove('shadow');
+    }
+}
+
 function openClose() {
     navContainer.classList.toggle('open-close-nav');
     navContainer_2.classList.toggle('open-close-nav');    
 }
+
+// close the navbar when close outside it
+document.addEventListener('click', function(e) {
+
+    if (
+        e.target.closest('.menu-btn') || e.target.closest('.navbar-container') ||
+        e.target.closest('.nav-container') || e.target.closest('nav')
+) return;
+
+    navContainer.classList.remove('open-close-nav');
+    navContainer_2.classList.remove('open-close-nav');
+})
